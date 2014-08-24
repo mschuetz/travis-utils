521f0d99cc94e4197389d436632d3eebc3a5ab8c
Matthias Schuetz <mschuetz@gmail.com>
2014-08-24 23:17:38 +0200

    fixed. check if remote branch exists

--- push_gh_pages.sh
+++ push_gh_pages.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -eu
 
-git checkout --orphan gh-pages || git checkout gh-pages
 REPO=$(git config remote.origin.url | sed -e 's/git:/https:/')
 git remote set-url --push origin $REPO
+
+export NEW=0
+git fetch -q origin gh-pages || export NEW=1
+if [ $NEW -eq 1 ]; then
+	# create branch
+	git checkout --orphan gh-pages
+else
+    # use existing
+    git fetch -q origin gh-pages || true
+	git fetch origin gh-pages
+	git checkout FETCH_HEAD
+	git checkout -b travis
+fi
+
 git remote set-branches --add origin gh-pages
-git fetch -q
+
 git config user.name "$NAME"
 git config user.email "$EMAIL"
 git config credential.helper "store --file=.git/credentials"
 echo "https://$GH_TOKEN:@github.com" > .git/credentials
-git pull
 
 FILES=$(find $REPORT_DIR/* | sed -e "s|$REPORT_DIR/||")
 cp -prf $REPORT_DIR/* .
